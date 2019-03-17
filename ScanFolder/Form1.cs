e3f1f1e7cf2421a5c8a6fca5e82cd053ca469314
kertak90 <35306203+kertak90@users.noreply.github.com>
2019-03-17 18:29:48 +0300

    Приложение для составления списка содержимого данной папки и вложенных папок

---
+++ ScanFolder/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScanFolder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "ScanFolder";
+            label1.Text = "Выберите папку!";
+            button1.Text = "Выбрать папку";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string Path;
+            OpenFileDialog FD = new OpenFileDialog();
+            FolderBrowserDialog FB = new FolderBrowserDialog();
+            FB.ShowDialog();
+            Path = FB.SelectedPath;
+            //FD.ShowDialog();
+            //Path = FD.FileName;
+            if(Path!=null&&Path!="")
+            {
+                ScanFolder.StartScan(Path);
+                label1.Text = "Список содержимого создан!";
+            }            
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ScanFolder.Close();
+        }
+    }
+}
---
+++ ScanFolder/ScanFolder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ScanFolder
+{
+    static class ScanFolder
+    {
+        static FileStream fs;
+        static StreamWriter sw;
+        static int index = 0;                                                   //Индекс числа файлов
+
+        public static void StartScan(string path)
+        {
+            string filename = path + "\\FILELIST.txt";
+            fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            sw = new StreamWriter(fs);
+            DirectoryInfo dir = new DirectoryInfo(path);                        //dir содержит информацию о данной директории
+            DateTime today = DateTime.Now;
+
+            sw.WriteLine("Дата создания списка:\t{0}", today);
+            sw.WriteLine("№\tДата изменения \t\tПуть до файла");                                                    //распечатка информации о данной директории
+            printDirect(dir);                                                   //метод создающий дерево каталога                                 
+        }     
+        
+        static ScanFolder()
+        {
+
+        }
+
+        private static void printFiles(DirectoryInfo dirFile)
+        {
+            FileInfo[] subFiles = dirFile.GetFiles();         
+            foreach (FileInfo f in subFiles)
+            {
+                index++;                
+                sw.WriteLine("{0}\t{1} \t{2}", index, f.CreationTime, f.FullName);
+            }
+            subFiles = null;
+        }
+
+        private static void printDirect(DirectoryInfo dir)
+        {            
+            DirectoryInfo[] subDirects = dir.GetDirectories();                  //создание массива директорий, dir.GetDirectories() - Возвращает подкаталоги текущего каталога                                           
+            printFiles(dir);                                                    //распечатка всех файлов данного каталога
+            foreach (DirectoryInfo d in subDirects)                             //перебор всех каталогов данного каталога
+            {
+                printDirect(d);                                                 //Снова вызываем метод который распечатает содержимое папки
+            }
+        }
+
+        public static void Close()
+        {
+            sw.Close();
+            fs.Close();
+        }
+    }
+}
